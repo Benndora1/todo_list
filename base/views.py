1fa1e72392cfa03dd6077bd8aaf9111294f6ce76
benndora1 <benndora2@gmail.com>
2022-06-08 14:03:59 +0300

    admin

--- base/views.py
+++ base/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
-
-class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=100)
-    complete = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
