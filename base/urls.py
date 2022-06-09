d5198c72c2673515675c299f2fe76ed364efffea
benndora1 <benndora2@gmail.com>
2022-06-09 08:07:02 +0300

    edit task

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateTask, TaskList, TaskDetail
+from .views import CreateTask, TaskList, TaskDetail, UpdateTask
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task_list'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task_update/<int:pk>/', UpdateTask.as_view(), name='task_update'),
     path('create_task/', CreateTask.as_view(), name='create_task'),
 ]
