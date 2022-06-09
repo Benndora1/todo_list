7481f903ca0fdbcaea33d98e9e3785f1586aca61
benndora1 <benndora2@gmail.com>
2022-06-09 07:21:27 +0300

    create vie

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import CreateTask, TaskList, TaskDetail
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task_list'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task/create/', CreateTask.as_view(), name='create_task'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 
 from . models import Task
 
@@ -14,3 +17,9 @@ class TaskDetail(DetailView):
     model = Task
     template_name = 'base/task.html'
     context_object_name = 'task'
+
+class CreateTask(CreateView):
+    model = Task
+    template_name = 'base/create_task.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
